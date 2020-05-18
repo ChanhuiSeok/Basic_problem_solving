de8bef57b16697a96c2af297bc94430f6736b020
Chanhui Seok <chanhuicom@gmail.com>
2020-05-18 21:56:32 +0900

    10816-숫자카드2

---
+++ [4]BinarySearch/10816-숫자카드2.cpp
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+map<int, int> maps;
+
+int main() {
+
+	int N, M, input;
+	bool flag = false;
+	vector<int> cards;
+	vector<int> candidate;
+	vector<int> result;
+
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++){
+		scanf("%d", &input);
+		cards.push_back(input);
+		maps[input]++;
+	}
+
+	scanf("%d", &M);
+	for (int i = 0; i < M; i++) {
+		scanf("%d", &input);
+		candidate.push_back(input);
+	}
+	
+	//(1) sorting
+	sort(cards.begin(), cards.end());
+
+	for (int idx = 0; idx < M; idx++) {
+		int left = 0;
+		int right = cards.size() - 1;
+		flag = false;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			int target = candidate[idx];
+
+			//중간값보다 타겟이 클 경우
+			if (cards[mid] < target) {
+				left = mid + 1;
+			}
+			else if (cards[mid] > target) {
+				right = mid - 1;
+			}
+			else {
+				flag = true;
+				break;
+			}
+		}
+		if (flag) result.push_back(maps[candidate[idx]]);
+		else result.push_back(0);
+	}
+
+	for (int i = 0; i < M; i++) {
+		printf("%d ", result[i]);
+	}
+
+}
